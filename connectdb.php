c0d35455e0fe20283d1da8b2666dc6f44774a2ad
Paul Tonning <tonningp@users.noreply.github.com>
2018-05-25 06:36:56 -0700

    Add files via upload

---
+++ connectdb.php
@@ -0,0 +1,12 @@
+<?php
+
+function connectdb($servername,$username,$password,$dbname)
+{
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+	return $conn;
+}
